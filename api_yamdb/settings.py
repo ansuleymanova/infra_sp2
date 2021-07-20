5a66decf64a0aea83d745be29066a93dc47c15b5
ansuleymanova <suleymanova.dev@gmail.com>
2021-07-20 23:37:06 +0300

    v1.1

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from datetime import timedelta
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'api_yamdb.settings'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
